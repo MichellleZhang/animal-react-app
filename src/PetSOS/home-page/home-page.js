bd0b2284826dc9e5a3f516368bd97d855bcc94ee
daisylin927 <daisylin927@gmail.com>
2023-06-26 17:01:41 -0700

    homepage update

--- src/PetSOS/home-page/home-page.js
+++ src/PetSOS/home-page/home-page.js
@@ -2,23 +2,47 @@ import { Link } from "react-router-dom";
 import styles from "./home-page.module.css";
 import PetList from "./components/petlist/petlist";
 import LostpetList from "./components/lostpet/lostpet"
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { getAnimal } from "../api/homepage";
+import { getLostPetList } from "../api/lostpet";
+import { useEffect } from "react";
+import { useState } from "react";
 // for testing purpose
-const HomePage=()=>{
+const HomePage = () => {
+    const [reponses, setReponses] = useState([]);
 
+    const fetchAllData = async () => {
+        const response = await axios.all([getAnimal("dog", "10001"), getLostPetList()]);
+        setReponses(response);
+    }
+
+    useEffect(() => {
+        fetchAllData();
+    }, [])
+    const { currentUser } = useSelector(state => state.user);
     return <div className={styles.container}>
         <div className={styles.bannerWrapper}>
-            <img src="/img/pet.jpg" alt=""/>
+            <img src="/img/pet.jpg" alt="" />
             <div className={styles.contextWrapper}>
                 <div className={styles.title}>Find Your Pet</div>
                 <div className={styles.subTitle}>Let Us to Bring Your Pet Home</div>
-                <Link to="/register">
-                    <div className={styles.signupButton}>Sign Up</div>
-                </Link>
-             </div>
+                {
+                    currentUser ? <div className={styles.subTitle}> Welcome</div>
+                        :
+                        <Link to="/register">
+                            <div className={styles.signupButton}>Sign Up</div>
+                        </Link>
+                }
+            </div>
         </div>
-        <PetList/>
-        <LostpetList/>
-    </div>;
+        {
+            reponses.length !== 0 && < div >
+                <PetList data={reponses[0]}/>
+                <LostpetList data={reponses[1]}/>
+            </div>
+        }
+    </div >;
 }
 
 export default HomePage;
